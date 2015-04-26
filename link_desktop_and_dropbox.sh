7e89d07f9d63128aae2326679567ee346dcd362f
Jose Varela <cabe56@users.noreply.github.com>
2015-04-25 19:20:20 -0500

    Added comment about symlink to desktop folder

--- link_desktop_and_dropbox.sh
+++ link_desktop_and_dropbox.sh
@@ -2,6 +2,8 @@
 # your Dropbox/Desktop folder will have files.
 # Create a temp backup by changin folder name
 mv -r ~/Dropbox/Desktop ~/Dropbox/Desktop.backup
+# Dropbox client will read symlink as a folder and
+# sync its contents
 ln -s ~/Desktop ~/Dropbox/Desktop
 # Move all files to new directory
 mv ~/Dropbox/Desktop.backup/* ~/Dropbox/Desktop
